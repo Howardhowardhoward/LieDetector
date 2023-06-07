abc148ad5d8656b0b58d8677e09c24de1fd78603
Howard.howard.howard <Howard.L.Malc@gmail.com>
2023-06-07 14:52:40 -0400

    Fake Data creation started

--- machine-learning/blooby.py
+++ machine-learning/blooby.py
@@ -2,3 +2,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+
+def createPulseData():
+    blank=np.linspace(65,85,50)
+    pulse=np.sin(blank)
+    plt.plot(np.linspace(0,50),pulse)
+
+createPulseData()
